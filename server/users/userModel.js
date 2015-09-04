cd3ae7523433115e050d2b305cba4686b66869bc
mtcrushmore <ryanthesmith@gmail.com>
2015-09-03 20:01:56 -0700

    created mongodb database with mongoose, user schema, and method to validate passwords using bcrypt

---
+++ server/users/userModel.js
@@ -0,0 +1,67 @@
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs')
+
+var UserSchema = new Schema ({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  type: {
+    type: String,
+    required: true
+  },
+  contactInfo: {
+    type: Object,
+    required: true
+  },
+  websiteUrl: {
+    type: String
+  },
+  additional: {
+    type: Object
+  },
+  connections: {
+    type: Array
+  },
+  foodData: {
+    type: Object
+  }
+  salt: String
+  //add messages/notifications field to/from foodbanks
+});
+
+//set up method to compare login password with stored encrypted password
+UserSchema.methods.verifyPassword = function(attemptedPassword) {
+  var savedPassword = this.password;
+  bcrypt.compare(attemptedPassword, savedPassword, function(err, match) {
+    callback(match);
+  })
+}
+
+//before a new user is saved to database, create encrypted password using bcrypt
+UserSchema.pre('save', function(next) {
+  var user = this;
+  if (!user.isModified('password')) {
+    return next();
+  } 
+
+  //generate the saltiest of salts 
+  bcrypt.genSalt(function(err, result) {
+    if (err) {
+      return next(err);
+    }
+    user.salt = result;
+  })
+
+  //encrypt the user's inputted password with the generated salt mixed in to make it nice and savory
+  bcrypt.hash(user.password, user.salt, null, function(err, hash) {
+    user.password = hash;
+    next();
+    })
+  }
+})
