704c4a53e4ab632e92eb2dbd499d3a109642f3a7
ccheung0926 <cindycheung0926@gmail.com>
2015-09-04 13:52:51 -0700

    setup server POST req

--- server/server.js
+++ server/server.js
@@ -4,9 +4,20 @@ var app = express();
 var path = require('path');
 // mongoose.connect('mongob://user:pass@localhost/api');
 
-// app.use(express.static(__dirname + '../client'));
 app.use(express.static(path.join(__dirname, '../client')));
 
+app.post('/signup:type', function(req, res){
+	var type = req.params.type;
+	// res.json({token: token});
+	var data = req.body;
+	//send data to db
+});
+app.post('/login', function(req, res){
+	//req.body--> my username and pw
+	  //send a reqest to db to get the token
+	  //if token is cool, res 201 or 202
+	  //else do sth
+});
 var port =  process.env.PORT || 3000; 
 var server = app.listen(port, function(){
   var host = server.address().address;
