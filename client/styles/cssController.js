cf1453235eab2c334d1494cd45f2196756f8e3d4
Tatsumi Tagawa <tatsumi.tagawa@gmail.com>
2015-09-08 10:37:07 -0700

    dummy files for testing ngHref

---
+++ client/styles/cssController.js
@@ -0,0 +1,11 @@
+angular.module('linkApp', [])
+  .controller('cssController', function($scope) {
+
+    $scope.layout = 'test1';
+
+    //$scope.layouts = [
+      //{ name: 'test1', url: 'test1' },
+      //{ name: 'test2', url: 'test2' },
+      //{ name: 'test3', url: 'test3' }
+    //];
+  });
