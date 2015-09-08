2a1268ad5357bcee2368164609bef7019eb4c79f
Tatsumi Tagawa <tatsumi.tagawa@gmail.com>
2015-09-08 15:58:46 -0700

    Revert "(update) created initial rst and fbk views "

--- client/app/restaurants/rst.js
+++ client/app/restaurants/rst.js
@@ -1,17 +1,7 @@
 angular.module('myApp.restaurants', [])
 
-.controller('RestaurantsCtrl', ['$scope','AjaxService', '$routeParams', function($scope, AjaxService, $routeParams) {
+.controller('RestaurantsCtrl', [function() {
   var vm = this;
-  var userId = $routeParams.type;
-  $scope.rstInfo = { };
-  $scope.counter = { count: 0 };
 
-  function getRstData(userId) {
-    return AjaxService.getRstData(userId).success(function(data){
-      $scope.rstInfo = data; //expect data to be {username: 1, restaurantname: 'in n out'}
-    });
-  }  
 
-  getRstData(userId);
-
-}]);
+}])
