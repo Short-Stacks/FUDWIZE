4d28557754cb2602f8ac921370dc43e190cfaf43
ccheung0926 <cindycheung0926@gmail.com>
2015-09-09 12:47:09 -0700

    (updated) dashboard udpated

--- client/app/dashboard/dash.js
+++ client/app/dashboard/dash.js
@@ -1,7 +1,16 @@
 angular.module('myApp.dashboard', [])
 
-.controller('DashboardCtrl', [function() {
+.controller('DashboardCtrl', ['$scope', 'AjaxService', '$routeParams', function($scope, AjaxService, $routeParams) {
   var vm = this;
+  var userId = $routeParams.type;
+  $scope.dashInfo = {};
 
-
+  function getDashData(userId){
+    return AjaxService.getDashData(userId).success(function(data){
+      $scope.dashInfo = data.rst; 
+      //data.rst is an array-object
+      // data.rst[0].contactInfo ... blah blah
+    });
+  }
+  getDashData(userId);
 }])
