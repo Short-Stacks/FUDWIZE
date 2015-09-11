c1dbab6f4950acf7a731892610270eb5dadf2391
ccheung0926 <cindycheung0926@gmail.com>
2015-09-10 22:22:34 -0700

    pulling upstream

--- client/app/dashboard/dash.js
+++ client/app/dashboard/dash.js
@@ -3,8 +3,27 @@ angular.module('myApp.dashboard', [])
 .controller('DashboardCtrl', ['$routeParams', 'AjaxService', function($routeParams, AjaxService) {
   var vm = this;
   vm.username = $routeParams.username;
+
   AjaxService.getDashboardData(vm.username)
     .then(function(data){
-      console.log(data);
+      //dashboard for foodbank so far
+      vm.data = data;
+      console.log(vm.data);
+      // { rst: [{},{},{},{}],
+ // fbk: {name: }
     });
 }]);
+
+(function(){
+  angular.module('myApp.dashboard').directive('scrolling', scrolling);
+
+  function scrolling(){
+    return{
+      scope:{
+        shouldScroll: '=scrolling',
+        direction: '@direction'
+      },
+      link: function(scope, element, attrs)
+    }
+  }
+})
