6c4ab5cb3d321fb4ae527dff99200cd7b25f68f7
ccheung0926 <cindycheung0926@gmail.com>
2015-09-12 12:00:57 -0700

    (updated)added Restaurants' scrollbar and added filter on dash.html and dash.js

--- client/app/dashboard/dash.js
+++ client/app/dashboard/dash.js
@@ -5,25 +5,38 @@ angular.module('myApp.dashboard', [])
   vm.username = $routeParams.username;
 
   AjaxService.getDashboardData(vm.username)
-    .then(function(data){
-      //dashboard for foodbank so far
-      vm.data = data;
-      console.log(vm.data);
-      // { rst: [{},{},{},{}],
- // fbk: {name: }
+    .then(function(data) {
+      console.log('data', data);
+      vm.rst = data.rst;
+      console.log('vm.rst', vm.rst);
+      // on each vm.rst object element, it contains foodData object
+      // under foodData, it contains all foodType, mealType..etc, for example:
+      // foodData: { foodType:{canned Goods: true, bakedGoods: false..etc}}
+      // if I only used ng-repeat to print out all the info on the html, if will cause some 
+      // issues for the filter 
+      // therefore, I declare the property under foodData directly under the vm.rst object
+      for( var i = 0; i < vm.rst.length; i++ ) {
+        console.log('i', vm.rst[i]);
+        var foodType = objectForloop(vm.rst[i].foodData.foodType);
+        var mealType = objectForloop(vm.rst[i].foodData.mealType);
+        var pickupTime = objectForloop(vm.rst[i].foodData.pickupTime);
+        var pickupDay = objectForloop(vm.rst[i].foodData.pickupDay);
+
+        vm.rst[i].foodType = foodType;
+        vm.rst[i].mealType = mealType;
+        vm.rst[i].pickupTime = pickupTime;
+        vm.rst[i].pickupDay = pickupDay;
+      }
     });
-}]);
-
-(function(){
-  angular.module('myApp.dashboard').directive('scrolling', scrolling);
-
-  function scrolling(){
-    return{
-      scope:{
-        shouldScroll: '=scrolling',
-        direction: '@direction'
-      },
-      link: function(scope, element, attrs)
+  var objectForloop = function(obj) {
+    var array = [];
+    for( var key in obj ) {
+      if( obj[key] ){
+        array.push(key);
+        array.push(", ");
+      }
     }
+    array.pop();
+    return array.join("");
   }
-})
+}])
