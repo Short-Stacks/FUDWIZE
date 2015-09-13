8705ca7e366477627ad73c3ddb47f0027df330c7
Tatsumi Tagawa <tatsumi.tagawa@gmail.com>
2015-09-12 20:24:24 -0700

    (update) Data changed to reflect test cases

--- server/users/testUsers.js
+++ server/users/testUsers.js
@@ -67,82 +67,188 @@ var createTestUsers = function(hashPassword) {
       password: hashPassword,
       type: 'rst',
       contactInfo: {
-        name: 'Chipotle',
-        phoneNumber: "415-555-TACO",
+        name: 'Chipotle Mexican Grill',
+        phoneNumber: '6505442950',
         email: 'chipotle@chipotle.com',
-        streetName: '49 Crystal Springs Road',
-        cityStreetZip: 'San Mateo, CA 94023'
+        streetName: '251 E 3rd Ave',
+        cityStreetZip: 'San Mateo, CA 94402'
       },
       websiteUrl: 'www.chipotle.com',
       additional: {
-        aboutUs: 'We make yummy burritos.'
+        aboutUs: "When Chipotle opened its first restaurant in 1993, the idea was simple: show that food served fast didn't have to be a “fast-food” experience. Using high-quality raw ingredients, classic cooking techniques, and distinctive interior design, we brought features from the realm of fine dining to the world of quick-service restaurants."
       },
       connections: [],
-      foodData: {},
+      foodData: {
+
+        mealType : {
+          'Breakfast': false,
+          'Lunch': false,
+          'Dinner': true,
+          'Dessert': false,
+        },
+
+        foodType : {
+          'Baked Goods': true,
+          'Produce': true,
+          'Canned Goods': true,
+          'Meats': true,
+          'Dairy': false,
+        },
+
+        pickupDay : {
+          'Monday': false,
+          'Tuesday': true,
+          'Wednesday': false,
+          'Thursday': true,
+          'Friday': false,
+          'Saturday': true,
+          'Sunday': false,
+        },
+
+        pickupTime : {
+          'Early Morning (6AM-9AM)': false,
+          'Late Morning (9AM-12PM)': true,
+          'Early Afternoon (12PM-3PM)': false,
+          'Late Afternoon (3PM-6PM)': true,
+          'Evening (6PM-9PM)': false,
+        }
+
+      },
     });
     chipotle.save();
   });
 
-  hashPassword('mehfils', function(hashPassword) {
-    var mehfils = new User({
-      username: 'mehfils',
+  hashPassword('mehfil', function(hashPassword) {
+    var mehfil = new User({
+      username: 'mehfil',
       password: hashPassword,
       type: 'rst',
       contactInfo: {
-        name: 'Mehfils',
-        phoneNumber: "415-555-LAMB",
-        email: 'mehfils@mehfils.com',
-        streetName: '284 Corny Court',
-        cityStreetZip: 'Livermore, CA 94023'
+        name: 'Mehfils Indian Cuisine',
+        phoneNumber: '4158969000',
+        email: 'info@mehfilindiancuisine.us',
+        streetName: '28 2nd Street',
+        cityStreetZip: 'San Francisco, CA 94105'
       },
-      websiteUrl: 'www.mehfils.com',
+      websiteUrl: 'www.mehfilindiancuisine.us',
       additional: {
-        aboutUs: 'Have you tried our goat?'
+        aboutUs: "Mehfil Indian Restaurants offer the authentic flavors of Indian home cooking in two warmly adorned locations. Mehfil is an Indian Urdu word which means the gathering of family members, relatives, friends & colleagues sharing good moments, good food and having great fun. Mehfil is widely praised as one of one of San Francisco's top Indian dining destinations, just steps from San Franciscos' Financial District. 2011 brought another milestone for Mehfil with its' second Restaurant with Full Bar that opened in Pacific Heights."
       },
       connections: [],
-      foodData: {},
+      foodData: {
+
+        mealType : {
+          'Breakfast': false,
+          'Lunch': false,
+          'Dinner': true,
+          'Dessert': true,
+        },
+
+        foodType : {
+          'Baked Goods': false,
+          'Produce': false,
+          'Canned Goods': false,
+          'Meats': true,
+          'Dairy': true,
+        },
+
+        pickupDay : {
+          'Monday': false,
+          'Tuesday': false,
+          'Wednesday': true,
+          'Thursday': false,
+          'Friday': false,
+          'Saturday': true,
+          'Sunday': false,
+        },
+
+        pickupTime : {
+          'Early Morning (6AM-9AM)': false,
+          'Late Morning (9AM-12PM)': false,
+          'Early Afternoon (12PM-3PM)': false,
+          'Late Afternoon (3PM-6PM)': false,
+          'Evening (6PM-9PM)': true,
+        }
+
+      }
     });
-    mehfils.save();
+    mehfil.save();
   });
 
-  hashPassword('steffs', function(hashPassword) {
-    var steffs = new User({
-      username: 'steffs',
+  hashPassword('tacoshop', function(hashPassword) {
+    var tacoshop = new User({
+      username: 'tacoshop',
       password: hashPassword,
       type: 'rst',
       contactInfo: {
-        name: 'Steffs Bar',
-        phoneNumber: "415-555-BEER",
-        email: 'steffs@steffs.com',
-        streetName: '431 Armor Hot Dog Lane',
-        cityStreetZip: 'San Ramon, CA 94023'
+        name: 'The Taco Shop at Underdogs',
+        phoneNumber: '4155668700',
+        email: 'www.tacoshopsf.com',
+        streetName: '1824 Irving Street',
+        cityStreetZip: 'San Francisco, CA 94122'
       },
-      websiteUrl: 'www.steffs.com',
+      websiteUrl: 'www.tacoshopsf.com',
       additional: {
-        aboutUs: 'We have hot dogs. Mostly beer though.'
+        aboutUs: 'Our goal is to provide local, fresh and sustainable food to the best of our abilities. We care about you, our food and the future of our world.'
       },
       connections: [],
-      foodData: {},
+      foodData: {
+
+        mealType : {
+          'Breakfast': false,
+          'Lunch': false,
+          'Dinner': true,
+          'Dessert': true,
+        },
+
+        foodType : {
+          'Baked Goods': true,
+          'Produce': false,
+          'Canned Goods': false,
+          'Meats': true,
+          'Dairy': false,
+        },
+
+        pickupDay : {
+          'Monday': false,
+          'Tuesday': false,
+          'Wednesday': true,
+          'Thursday': false,
+          'Friday': false,
+          'Saturday': true,
+          'Sunday': false,
+        },
+
+        pickupTime : {
+          'Early Morning (6AM-9AM)': true,
+          'Late Morning (9AM-12PM)': false,
+          'Early Afternoon (12PM-3PM)': false,
+          'Late Afternoon (3PM-6PM)': false,
+          'Evening (6PM-9PM)': true,
+        }
+
+      },
     });
-    steffs.save();
+    tacoshop.save();
   });
 
-  hashPassword('sfFoodBank', function(hashPassword) {
+  hashPassword('sffoodbank', function(hashPassword) {
     var sfFoodBank = new User({
-      username: 'sfFoodBank',
+      username: 'sffoodbank',
       password: hashPassword,
       type: 'fbk',
       contactInfo: {
-        name: 'SF Food Bank',
-        phoneNumber: "415-555-BANK",
+        name: 'SF Marin Food Bank',
+        phoneNumber: '4152821900',
         email: 'sfFoodBank@sfFoodBank.org',
-        streetName: '943 Major Uphill Street',
-        cityStreetZip: 'San Francisco, CA 94023'
+        streetName: '900 Pennsylvania Avenue',
+        cityStreetZip: 'San Francisco, CA 94107',
+        serviceArea: 'San Francisco and Marin County'
       },
       websiteUrl: 'www.sfFoodBank.org',
       additional: {
-        aboutUs: 'We need some food yo.',
-        mission: 'Secretly let dogs take over the world.'
+        aboutUs: "The Food Bank is a lifeline. Every day we distribute enough food for 107,000 meals — that's more than 46 million pounds this year. It takes a comprehensive approach and a network of 450 partners to make it happen.",
+        mission: "To end hunger in San Francisco and Marin."
       },
       connections: [],
       foodData: {},
@@ -150,50 +256,50 @@ var createTestUsers = function(hashPassword) {
     sfFoodBank.save();
   });
 
-  hashPassword('oaklandFoodBank', function(hashPassword) {
-    var oaklandFoodBank = new User({
-      username: 'oaklandFoodBank',
+  hashPassword('alamedafbk', function(hashPassword) {
+    var alamedaCountyFoodBank = new User({
+      username: 'alamedafbk',
       password: hashPassword,
       type: 'fbk',
       contactInfo: {
-        name: 'Oakland Food Bank',
-        phoneNumber: "415-555-FOOD",
-        email: 'oaklandFoodBank@sfFoodBank.org',
-        streetName: '34 Royale Cheese Lane',
-        cityStreetZip: 'Oakland, CA 93012'
+        name: 'Alameda County Community Food Bank',
+        phoneNumber: '5106353663',
+        email: 'info@accfb.org',
+        streetName: '7900 Edgewater Drive',
+        cityStreetZip: 'Oakland, CA 94621'
       },
-      websiteUrl: 'www.oaklandFoodBank.org',
+      websiteUrl: 'www.accfb.org',
       additional: {
-        aboutUs: 'Screw SF Food Bank. Hook us up.',
-        mission: 'Creating our own Batman'
+        aboutUs: "Alameda County Community Food Bank has been in business since 1985 ... with a vision toward a day when we can go out of business. We are the hub of a vast collection and distribution network that provides food for 240 nonprofit agencies in Alameda County. In 2014, the Food Bank distributed 25 million meals -- more than half of the food was fresh fruits and vegetables. Since moving into our permanent facility near the Oakland Airport in 2005 and leading the national food bank movement for a ban on the distribution of carbonated beverages, the Food Bank has ramped up distribution of fresh fruits and vegetables by more than 1,000%.",
+        mission: "Alameda County Community Food Bank passionately pursues a hunger-free community"
       },
       connections: [],
       foodData: {},
     });
-    oaklandFoodBank.save();
+    alamedaCountyFoodBank.save();
   });
 
-  hashPassword('sausalitoFoodBank', function(hashPassword) {
-    var sausalitoFoodBank = new User({
-      username: 'sausalitoFoodBank',
+  hashPassword('communityActionNapaValley', function(hashPassword) {
+    var communityActionNapaValley = new User({
+      username: 'communityActionNapaValley',
       password: hashPassword,
       type: 'fbk',
       contactInfo: {
-        name: 'Sausalito Food Bank',
-        phoneNumber: "415-555-FOOD",
-        email: 'sausalitoFoodBank@sfFoodBank.org',
-        streetName: '321 Beach Boulevard',
-        cityStreetZip: 'Sausalito, CA 94023'
+        name: 'Community Action Napa Valley',
+        phoneNumber: '7072536100',
+        email: 'canv@can-v.org',
+        streetName: '2310 Laurel Street #1',
+        cityStreetZip: 'Napa, CA 94559'
       },
-      websiteUrl: 'www.sausalitoFoodBank.org',
+      websiteUrl: 'www.canv.org',
       additional: {
-        aboutUs: 'Fine foods only.',
-        mission: 'Seals also welcome'
+        aboutUs: 'Community Action stays true to its mission by continuing provide basic needs – food and shelter – along with other programs to support people as they become self-sufficient members of our vibrant community. We believe in partnership and are founding members of the Coalition of Non-Profit Organizations. We are active with many local initiatives and work groups including the Continuum of Care; Live Healthy Napa Valley; Area Agency on Aging; the Workforce Investment Board, and more to improve the quality of life for people throughout the Napa Valley.',
+        mission: "At Community Action of Napa Valley we believe that no one in our community should live in poverty. Every day we offer local people emergency assistance along with tools and skills to rebuild their lives and fully participate in this amazing community."
       },
       connections: [],
       foodData: {},
     });
-    sausalitoFoodBank.save();
+    communityActionNapaValley.save();
   });
 };
 
