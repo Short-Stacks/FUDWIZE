9296292b4c8f6faa3dff70ac16fbd5cd27c2e35d
ccheung0926 <cindycheung0926@gmail.com>
2015-09-14 10:43:30 -0700

    (fixed) prevent form kicking user back to profile page when clicking invalid address

--- client/app/dashboard/dash.js
+++ client/app/dashboard/dash.js
@@ -137,11 +137,11 @@ angular.module('myApp.dashboard', [])
         });
         geocoder.geocode({ 'address': address }, function(results, status) {
           if (status === google.maps.GeocoderStatus.OK) {
-          map.setCenter(results[0].geometry.location);
-          var marker = new google.maps.Marker({
-            map: map,
-            position: results[0].geometry.location
-          });
+            map.setCenter(results[0].geometry.location);
+            var marker = new google.maps.Marker({
+              map: map,
+              position: results[0].geometry.location
+            });
             google.maps.event.addListener(marker, 'click', (function(marker, contentString, infowindow) {
               return function() {
                 infowindow.setContent(contentString);
@@ -149,9 +149,10 @@ angular.module('myApp.dashboard', [])
               }
             })(marker, contentString, infowindow));
           } 
-        else {
-          alert('Geocode was not successful for the following reason: ' + status);
-        }
+          else {
+            alert('Geocode was not successful for the following reason: ' + status);
+            event.preventDefault();
+          }
       });
       event.preventDefault();
     }
