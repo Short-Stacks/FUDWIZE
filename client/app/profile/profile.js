922a75ace67d6a7fe147d771ff75cb24d69efffc
Tatsumi Tagawa <tatsumi.tagawa@gmail.com>
2015-09-13 21:11:31 -0700

    (update) Changed profile update notification
      - The notification the user gets when the "Update Profile" was confusing. Previously when the button was pressed, the text inside the button changed to "Profile Updated!" but it was still a button that did nothing.
      - It now shows profile has been updated for two seconds and goes away.
        User can update it again using the same button.
      - Removed HTML validation error of having two buttons that were "submit."
      - Fixed indentation

--- client/app/profile/profile.js
+++ client/app/profile/profile.js
@@ -1,33 +1,37 @@
 angular.module('myApp.profile', [])
 
-  .controller('ProfileCtrl', ['$routeParams', 'AjaxService', function($routeParams, AjaxService) {
-    var vm = this;
-    vm.type = $routeParams.type;
-    vm.username = $routeParams.username;
-    vm.connections;
+.controller('ProfileCtrl', ['$routeParams', '$timeout', 'AjaxService', function($routeParams, $timeout, AjaxService) {
+  var vm = this;
+  vm.type = $routeParams.type;
+  vm.username = $routeParams.username;
+  vm.connections;
 
-    AjaxService.getProfileData(vm.type, vm.username)
-      .then(function(data){
-        console.log(data);
-        vm.data = data;
-      })
-      .then(function() {
-        if (vm.type == 'fbk') {
-          //make AJAX request for username's in foodbank's connections array
-          AjaxService.getConnectionsData(vm.type, vm.username)
-              .then(function(data) {
-                vm.connections = data.data;
-              })
-          }; 
-        });
+  AjaxService.getProfileData(vm.type, vm.username)
+  .then(function(data){
+    console.log(data);
+    vm.data = data;
+  })
+  .then(function() {
+    if (vm.type == 'fbk') {
+      //make AJAX request for username's in foodbank's connections array
+      AjaxService.getConnectionsData(vm.type, vm.username)
+      .then(function(data) {
+        vm.connections = data.data;
+      });
+    }
+  });
 
-    vm.updateProfile = function() {
-      console.log(vm.data);
-      AjaxService.updateProfileData(vm.data, vm.type, vm.username)
-        .then(function() {
-          // $route.reload();
-        });
-    };
+  vm.updateProfile = function() {
+    console.log(vm.data);
+    AjaxService.updateProfileData(vm.data, vm.type, vm.username)
+    .then(function() {
+      // $route.reload();
+    });
 
+    vm.updateConfirmation = "Profile Updated!";
+    $timeout(function() {
+      vm.updateConfirmation = "";
+    }, 2000);
+  };
 
 }]);
