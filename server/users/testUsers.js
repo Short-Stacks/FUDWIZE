4e0b1d576aeefd54fb4866f829b9dd7e884dd168
Tatsumi Tagawa <tatsumi.tagawa@gmail.com>
2015-09-14 14:51:42 -0700

    (update) added urls of logo

--- server/users/testUsers.js
+++ server/users/testUsers.js
@@ -91,6 +91,7 @@ var createTestUsers = function(hashPassword) {
         streetName: '81 Jefferson Street',
         cityStateZip: 'San Francisco, CA 94133'
       },
+      imageUrl: 'https://upload.wikimedia.org/wikipedia/en/0/0f/Johnny_Rockets_logo.png',
       websiteUrl: 'www.johnnyrockets.com',
       additional: {
         aboutUs: "Ronn Teitelbaum, an award-winning men's fashion retailer, launched the first Johnny Rockets on June 6, 1986, on Melrose Avenue, in Los Angeles, California. He founded the concept on the belief that everyone deserves a place where they can escape from today’s complicated world and experience the uncomplicated goodness of classic Americana. The name originated by combining the timeless Johnny Appleseed story with the classic Oldsmobile Rocket 88. Together, they embody the concept of classic Americana and the promise of the future"
@@ -149,6 +150,7 @@ var createTestUsers = function(hashPassword) {
         cityStateZip: 'San Francisco, CA 94103'
       },
       websiteUrl: 'turtletowersf.com',
+      imageUrl: 'https://pbs.twimg.com/profile_images/1226707242/Turtle_Logo_400x400.jpg',
       additional: {
         aboutUs: "From Turtle Tower in Hanoi ... to Turtle Tower Restaurant in San Francisco ..."
       },
@@ -206,6 +208,7 @@ var createTestUsers = function(hashPassword) {
         cityStateZip: 'San Francisco, CA 94105'
       },
       websiteUrl: 'www.chipotle.com',
+      imageUrl: 'https://upload.wikimedia.org/wikipedia/en/thumb/3/3b/Chipotle_Mexican_Grill_logo.svg/1024px-Chipotle_Mexican_Grill_logo.svg.png',
       additional: {
         aboutUs: "When Chipotle opened its first restaurant in 1993, the idea was simple: show that food served fast didn't have to be a “fast-food” experience. Using high-quality raw ingredients, classic cooking techniques, and distinctive interior design, we brought features from the realm of fine dining to the world of quick-service restaurants."
       },
@@ -263,6 +266,7 @@ var createTestUsers = function(hashPassword) {
         cityStateZip: 'San Francisco, CA 94105'
       },
       websiteUrl: 'www.mehfilindiancuisine.us',
+      imageUrl: 'http://mehfilindian.com/images/logo5b.jpg',
       additional: {
         aboutUs: "Mehfil Indian Restaurants offer the authentic flavors of Indian home cooking in two warmly adorned locations. Mehfil is an Indian Urdu word which means the gathering of family members, relatives, friends & colleagues sharing good moments, good food and having great fun. Mehfil is widely praised as one of one of San Francisco's top Indian dining destinations, just steps from San Franciscos' Financial District. 2011 brought another milestone for Mehfil with its' second Restaurant with Full Bar that opened in Pacific Heights."
       },
@@ -320,6 +324,7 @@ var createTestUsers = function(hashPassword) {
         cityStateZip: 'San Francisco, CA 94122'
       },
       websiteUrl: 'www.tacoshopsf.com',
+      imageUrl: 'https://pbs.twimg.com/profile_images/637390965792112640/c1hHnSTI_400x400.png',
       additional: {
         aboutUs: 'Our goal is to provide local, fresh and sustainable food to the best of our abilities. We care about you, our food and the future of our world.'
       },
@@ -378,6 +383,7 @@ var createTestUsers = function(hashPassword) {
         serviceArea: 'San Francisco and Marin County'
       },
       websiteUrl: 'www.sfFoodBank.org',
+      imageUrl: 'https://pbs.twimg.com/profile_images/471786942959349760/ASvpF1Pv_400x400.jpeg',
       additional: {
         aboutUs: "The Food Bank is a lifeline. Every day we distribute enough food for 107,000 meals — that's more than 46 million pounds this year. It takes a comprehensive approach and a network of 450 partners to make it happen.",
         mission: "To end hunger in San Francisco and Marin."
@@ -401,6 +407,7 @@ var createTestUsers = function(hashPassword) {
         cityStateZip: 'Oakland, CA 94621'
       },
       websiteUrl: 'www.accfb.org',
+      imageUrl: 'https://pbs.twimg.com/profile_images/378800000534210932/79383c558b9d24f7c8d6bd71623d3b7f_400x400.jpeg',
       additional: {
         aboutUs: "Alameda County Community Food Bank has been in business since 1985 ... with a vision toward a day when we can go out of business. We are the hub of a vast collection and distribution network that provides food for 240 nonprofit agencies in Alameda County. In 2014, the Food Bank distributed 25 million meals -- more than half of the food was fresh fruits and vegetables. Since moving into our permanent facility near the Oakland Airport in 2005 and leading the national food bank movement for a ban on the distribution of carbonated beverages, the Food Bank has ramped up distribution of fresh fruits and vegetables by more than 1,000%.",
         mission: "Alameda County Community Food Bank passionately pursues a hunger-free community"
@@ -424,6 +431,7 @@ var createTestUsers = function(hashPassword) {
         cityStateZip: 'Napa, CA 94559'
       },
       websiteUrl: 'www.canv.org',
+      imageUrl: 'https://fbcdn-profile-a.akamaihd.net/hprofile-ak-xpa1/v/t1.0-1/p160x160/529316_360664947373091_1073923377_n.jpg?oh=1e3e40a6d96b226665ad2e370df7a9bc&oe=566627C1&__gda__=1449732457_7b165c50cca1c8e92f0bcf7504e73e75',
       additional: {
         aboutUs: 'Community Action stays true to its mission by continuing provide basic needs – food and shelter – along with other programs to support people as they become self-sufficient members of our vibrant community. We believe in partnership and are founding members of the Coalition of Non-Profit Organizations. We are active with many local initiatives and work groups including the Continuum of Care; Live Healthy Napa Valley; Area Agency on Aging; the Workforce Investment Board, and more to improve the quality of life for people throughout the Napa Valley.',
         mission: "At Community Action of Napa Valley we believe that no one in our community should live in poverty. Every day we offer local people emergency assistance along with tools and skills to rebuild their lives and fully participate in this amazing community."
